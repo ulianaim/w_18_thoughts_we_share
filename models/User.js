f96a7c52c698d43743a68790f07347637e5768bb
ulianim <uliana_usa@yahoo.com>
2023-08-21 18:53:16 -0400

    New code was added to the files

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('user',userSchema);
+
+module.exports = User;
